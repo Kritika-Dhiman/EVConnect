2e183a85030b31269c57b96a35b79443e8e1123c
Kritika-Dhiman <144471110+Kritika-Dhiman@users.noreply.github.com>
2024-12-14 22:14:10 +0530

    Added reservation form

---
+++ js/booking.js
@@ -0,0 +1,20 @@
+const header=document.querySelector("header");
+const hamburger=document.querySelector(".hamburger-on");
+const navLinks=document.querySelector(".nav-links");
+let hamburgerOpen= false;
+
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",window.scrollY>0);
+})
+
+hamburger.addEventListener("click",function(){
+    if(!hamburgerOpen){
+        navLinks.classList.toggle("show-navlinks");
+        hamburger.classList.toggle("hamburger-off");
+    }else{
+        navLinks.classList.remove("show-navlinks");
+        hamburger.classList.remove("hamburger-off")
+        hamburgerOpen=false;
+    }
+})
+
